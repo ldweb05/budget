e1cb1d1c8cf932c62e8ff3d05c3e844bbbb3ae9b
Lorenzo Diana <ldweb05@gmail.com>
2026-07-28 20:30:40 +0200

    Completa migrazione autenticazione database

--- www/config.php
+++ www/config.php
@@ -1,15 +1,10 @@
 <?php
-// Credenziali di accesso all'applicazione
-define('USER_APP', getenv('APP_USER'));
-
-// Metti qui la tua password personalizzata al posto di 'ilmiobudget'
-define('PASS_APP', getenv('APP_PASSWORD'));
-
 // Funzione di controllo per proteggere le pagine
 function controlla_autenticazione() {
     if (session_status() == PHP_SESSION_NONE) {
         session_start();
     }
+
     if (!isset($_SESSION['loggato']) || $_SESSION['loggato'] !== true) {
         header("Location: login.php");
         exit;
